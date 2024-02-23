b0a2c187932cd22d3e36ae3a5ba71db1d0ed6207
xakerzz <xakerz@me.com>
2024-02-23 12:25:01 +0300

    new commit

--- src/main/java/com/xakerz/QrCode/QrCodeApplication.java
+++ src/main/java/com/xakerz/QrCode/QrCodeApplication.java
@@ -2,8 +2,10 @@ package com.xakerz.QrCode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.xakerz.QrCode")
 public class QrCodeApplication {
 
 	public static void main(String[] args) {
