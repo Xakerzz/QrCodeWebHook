b9313ec0bc13897af03112d275a680537f6065b3
xakerzz <xakerz@me.com>
2024-02-23 12:10:17 +0300

    new commit

--- src/main/java/com/xakerz/QrCode/QrCode.java
+++ src/main/java/com/xakerz/QrCode/QrCode.java
@@ -1,28 +1,55 @@
 package com.xakerz.QrCode;
 
-import org.springframework.context.annotation.Configuration;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import net.glxn.qrgen.QRCode;
+import net.glxn.qrgen.image.ImageType;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendAudio;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.lang.reflect.Method;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
-
-@Configuration
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class QrCode extends TelegramWebhookBot {
+    String botPath;
+    String botUsername;
+    String botToken;
 
 
+    int count = 0;
 
+    private final TelegramBotConfig telegramBotConfig = new TelegramBotConfig();
 
+    private TelegramFacade telegramFacade;
 
-    private final TelegramBotConfig telegramBotConfig;
-
-    public QrCode(TelegramBotConfig telegramBotConfig) {
-        this.telegramBotConfig = telegramBotConfig;
+    public QrCode(TelegramFacade telegramFacade, DefaultBotOptions options, SetWebhook setWebhook) {
+        super(options, String.valueOf(setWebhook));
+        this.telegramFacade = telegramFacade;
+    }
+    public QrCode(TelegramFacade telegramFacade, SetWebhook setWebhook) {
+        super(String.valueOf(setWebhook));
+        this.telegramFacade = telegramFacade;
     }
 
 
@@ -38,24 +65,131 @@ public class QrCode extends TelegramWebhookBot {
 
     @Override
     public  BotApiMethod<?> onWebhookUpdateReceived(Update update) {
+        Message message = update.getMessage();
 
+
+        if (update.hasMessage() && update.getMessage().hasText() && update.getMessage().getText().equals("/start")) {
+            long id = message.getChatId();
+
+
+            sendTextMessage(id, "Теперь можно отправить ссылку и бот сделает QrCode для неё.");
+
+
+        } else if (update.hasMessage() && update.getMessage().hasText() && !update.getMessage().getText().equals("/start")) {
+            long id = message.getChatId();
+            sendTextMessage(id, " ✅QrCode успешно создан");
+            doQrCode(id, update.getMessage().getText());
+            sendTextMessage(id, "Создадим еще один❔");
+            count++;
+            if ((count & 3) == 0){
+                sendTextMessage(id, "В благодарность ты можешь подписаться на два моих канала ---> https://t.me/CalmHorizons  и  ");
+            }
+        }
         return null;
     }
 
-    public void sendAudio(long chatId, String pathToAudioFile) {
+    private void messageText(Long chatId, String newTextForMessage, String newTextForButtonOne, String newTextForCallbackOne, String newTextForButtonTwo, String newTextForCallbackTwo) {
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+
+        sendMessage.setText(newTextForMessage);
+        sendMessage.setReplyMarkup(getInlineKeyboard(newTextForButtonOne, newTextForButtonTwo, newTextForCallbackOne, newTextForCallbackTwo));
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private InlineKeyboardMarkup getInlineKeyboard(String newTextForButtonOne, String newTextForButtonTwo, String newTextForCallbackOne, String newTextForCallbackTwo) {
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+        List<InlineKeyboardButton> row = new ArrayList<>();
+        List<InlineKeyboardButton> row1 = new ArrayList<>();
+
+
+        InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
+        inlineKeyboardButton.setText(newTextForButtonOne);
+        inlineKeyboardButton.setCallbackData(String.valueOf(newTextForCallbackOne));
+        row.add(inlineKeyboardButton);
+
+        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
+        inlineKeyboardButton1.setText(newTextForButtonTwo);
+        inlineKeyboardButton1.setCallbackData(String.valueOf(newTextForCallbackTwo));
+        row.add(inlineKeyboardButton1);
+
+
+        keyboard.add(row);
+        keyboard.add(row1);
+
+
+        markup.setKeyboard(keyboard);
+        return markup;
+    }
+
+    private void sendTextMessage(long chatId, String textMessage) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(textMessage);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private void sendAudio(long chatId, String pathToAudioFile) {
         SendAudio sendAudio = new SendAudio();
         sendAudio.setChatId(chatId);
-        sendAudio.setAudio(new InputFile("https://cdn.muzpan.net/?h=JGraYpdVSDQYtq9_hytYL8Pudc6lcqB3I8KLbiwE84hKFpkyoFJsTtR6pn6SOV7ylwB64qZydcr1zJFogDD0XDlo5lM\\"));
-        sendAudio.setTitle("Название аудио"); // Необязательно: название аудио
-        sendAudio.setCaption("Подпись к аудио"); // Необязательно: подпись к аудио
-
-        // Отправляем аудио
+        InputFile audio = new InputFile(pathToAudioFile);
+        sendAudio.setAudio(audio);
         try {
             execute(sendAudio);
         } catch (TelegramApiException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
+
+    private SendPhoto sendPhotoMessage(long chatId, File file) {
+        SendPhoto sendPhoto = new SendPhoto();
+        InputFile inputFile = new InputFile(String.valueOf(file));
+        sendPhoto.setChatId(chatId);
+        sendPhoto.setPhoto(inputFile);
+
+        return sendPhoto;
+    }
+
+    public void doQrCode(long id, String str) {
+
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            QRCode.from(str).withCharset(StandardCharsets.UTF_8.name()).withSize(250, 250).to(ImageType.PNG).writeTo(outputStream);
+
+            File file = File.createTempFile("qrcode", ".png");
+            try (FileOutputStream fos = new FileOutputStream(file)) {
+                outputStream.writeTo(fos);
+            }
+
+            SendPhoto sendPhoto = new SendPhoto();
+            sendPhoto.setChatId(id);
+            sendPhoto.setPhoto(new InputFile(file));
+
+            try {
+                execute(sendPhoto);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+            // Удаляем временный файл после отправки
+            file.delete();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 
 
     @Override
@@ -64,5 +198,13 @@ public class QrCode extends TelegramWebhookBot {
     }
 
 
+    public void setBotToken(String botToken) {
+    }
+
+    public void setBotUsername(String userName) {
+    }
+
+    public void setBotPath(String webHookPath) {
+    }
 }
 
--- src/main/java/com/xakerz/QrCode/WebhookController.java
+++ src/main/java/com/xakerz/QrCode/WebhookController.java
@@ -18,7 +18,6 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.generics.WebhookBot;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -33,145 +32,23 @@ import java.util.List;
 public class WebhookController {
 
 
-    private final QrCode bot;
+    private final QrCode telegramBot;
 
-    int count = 0;
+
 
     @Autowired
-    public WebhookController( QrCode bot) {
-
-        this.bot = bot;
+    public WebhookController( QrCode telegramBot) {
+        this.telegramBot = telegramBot;
     }
 
 
     @PostMapping("/")
     public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
 
-        Message message = update.getMessage();
-
-
-        if (update.hasMessage() && update.getMessage().hasText() && update.getMessage().getText().equals("/start")) {
-            long id = message.getChatId();
-
-
-            sendTextMessage(id, "Теперь можно отправить ссылку и бот сделает QrCode для неё.");
-
-
-        } else if (update.hasMessage() && update.getMessage().hasText() && !update.getMessage().getText().equals("/start")) {
-            long id = message.getChatId();
-            sendTextMessage(id, " ✅QrCode успешно создан");
-            doQrCode(id, update.getMessage().getText());
-            sendTextMessage(id, "Создадим еще один❔");
-count++;
-if ((count & 3) == 0){
-    sendTextMessage(id, "В благодарность ты можешь подписаться на два моих канала ---> https://t.me/CalmHorizons  и  ");
-}
-        }
-
-
-        return null;
+        return telegramBot.onWebhookUpdateReceived(update);
     }
 
-    private void messageText(Long chatId, String newTextForMessage, String newTextForButtonOne, String newTextForCallbackOne, String newTextForButtonTwo, String newTextForCallbackTwo) {
 
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(chatId);
-
-        sendMessage.setText(newTextForMessage);
-        sendMessage.setReplyMarkup(getInlineKeyboard(newTextForButtonOne, newTextForButtonTwo, newTextForCallbackOne, newTextForCallbackTwo));
-        try {
-            bot.execute(sendMessage);
-        } catch (TelegramApiException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    private InlineKeyboardMarkup getInlineKeyboard(String newTextForButtonOne, String newTextForButtonTwo, String newTextForCallbackOne, String newTextForCallbackTwo) {
-        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
-        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
-        List<InlineKeyboardButton> row = new ArrayList<>();
-        List<InlineKeyboardButton> row1 = new ArrayList<>();
-
-
-        InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
-        inlineKeyboardButton.setText(newTextForButtonOne);
-        inlineKeyboardButton.setCallbackData(String.valueOf(newTextForCallbackOne));
-        row.add(inlineKeyboardButton);
-
-        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
-        inlineKeyboardButton1.setText(newTextForButtonTwo);
-        inlineKeyboardButton1.setCallbackData(String.valueOf(newTextForCallbackTwo));
-        row.add(inlineKeyboardButton1);
-
-
-        keyboard.add(row);
-        keyboard.add(row1);
-
-
-        markup.setKeyboard(keyboard);
-        return markup;
-    }
-
-    private void sendTextMessage(long chatId, String textMessage) {
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(chatId);
-        sendMessage.setText(textMessage);
-        try {
-            bot.execute(sendMessage);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    private void sendAudio(long chatId, String pathToAudioFile) {
-        SendAudio sendAudio = new SendAudio();
-        sendAudio.setChatId(chatId);
-        InputFile audio = new InputFile(pathToAudioFile);
-        sendAudio.setAudio(audio);
-        try {
-            bot.execute(sendAudio);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private SendPhoto sendPhotoMessage(long chatId, File file) {
-        SendPhoto sendPhoto = new SendPhoto();
-        InputFile inputFile = new InputFile(String.valueOf(file));
-        sendPhoto.setChatId(chatId);
-        sendPhoto.setPhoto(inputFile);
-
-        return sendPhoto;
-    }
-
-    public void doQrCode(long id, String str) {
-
-        try {
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            QRCode.from(str).withCharset(StandardCharsets.UTF_8.name()).withSize(250, 250).to(ImageType.PNG).writeTo(outputStream);
-
-            File file = File.createTempFile("qrcode", ".png");
-            try (FileOutputStream fos = new FileOutputStream(file)) {
-                outputStream.writeTo(fos);
-            }
-
-            SendPhoto sendPhoto = new SendPhoto();
-            sendPhoto.setChatId(id);
-            sendPhoto.setPhoto(new InputFile(file));
-
-            try {
-                bot.execute(sendPhoto);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-            // Удаляем временный файл после отправки
-            file.delete();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
 
 
     @GetMapping
