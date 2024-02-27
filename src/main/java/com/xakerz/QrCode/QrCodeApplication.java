478efe47af565295ede73629ac63bd44bed3783f
xakerzz <xakerz@me.com>
2024-02-27 11:54:05 +0300

    LongPollingBot QrCodeApplication

--- src/main/java/com/xakerz/QrCode/QrCodeApplication.java
+++ src/main/java/com/xakerz/QrCode/QrCodeApplication.java
@@ -1,15 +1,21 @@
 package com.xakerz.QrCode;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-@SpringBootApplication
-@ComponentScan(basePackages = "com.xakerz.QrCode")
+
 public class QrCodeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QrCodeApplication.class, args);
+		try {
+			TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+			telegramBotsApi.registerBot(new QrCode());
+		} catch (TelegramApiException e) {
+
+			throw new RuntimeException(e);
+		}
 	}
 
 }
